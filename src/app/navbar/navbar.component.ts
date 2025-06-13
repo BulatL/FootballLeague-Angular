d504fca65a4165db13ddeb47d00f2334981386d8
Luka Bulatovic <luka@presles.co.za>
2025-06-13 18:39:43 +0200

    Navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, ElementRef, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -7,8 +7,21 @@ import { Component } from '@angular/core';
 })
 export class NavbarComponent {
   isMenuOpen = false;
+  constructor(private el: ElementRef, private renderer: Renderer2) {}
 
   toggleMenu() {
     this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    // Get the nativeElement of the navbar component
+    const navbar = this.el.nativeElement.querySelector('#navbar');
+
+    if (window.scrollY > 50) {
+      this.renderer.addClass(navbar, 'navbar-scrolled');
+    } else {
+      this.renderer.removeClass(navbar, 'navbar-scrolled');
+    }
   }
 }
